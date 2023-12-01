2c6f87efdc874b08593964e77df4f406d7d998f2
AkshatBan <aksfirebansal2@gmail.com>
2023-11-30 20:33:57 -0600

    Added restaurant profile page

---
+++ app/src/main/java/com/cs407/inventorizorapp/restaurant_profile.java
@@ -0,0 +1,14 @@
+package com.cs407.inventorizorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class restaurant_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_profile);
+    }
+}

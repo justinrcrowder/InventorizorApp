b9fdcf1a81c05c85b9e39b317cda4d2e43b07e93
TylerGee3 <gtyler3636@gmail.com>
2023-12-01 15:16:34 -0600

    See ingredient info page

---
+++ app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
@@ -0,0 +1,14 @@
+package com.cs407.inventorizorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngredientInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_info);
+    }
+}

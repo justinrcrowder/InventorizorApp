82965507231442433cc3972a29ea6140b8bb1500
TylerGee3 <gtyler3636@gmail.com>
2023-12-05 16:33:18 -0600

    Added RecipeInfo activity

--- app/src/main/java/com/cs407/inventorizorapp/RecipeInfo.java
+++ app/src/main/java/com/cs407/inventorizorapp/RecipeInfo.java
@@ -4,25 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class IngredientInfo extends AppCompatActivity {
+public class RecipeInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ingredient_info);
+        setContentView(R.layout.activity_recipe_info);
+    }
+
+    public void goToAddEditRecipe() {
+
     }
 
     public void goToRecipeInfo() {
 
     }
-
-    //Might need two activities for Adding and Editing recipes
-    public void goToAddEditRecipe() {
-
-    }
-
-    public void goToMainActivity() {
-
-    }
-
 }

826921070deb079d37643ca2fdb8933cdc5d88ce
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-06 10:45:30 -0600

    fixed back button for IngredientInfo

--- app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
+++ app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
@@ -4,13 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IngredientInfo extends AppCompatActivity {
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_info);
+
+        backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToMainActivity();
+            }
+        });
     }
 
     public void goToRecipeInfo() {

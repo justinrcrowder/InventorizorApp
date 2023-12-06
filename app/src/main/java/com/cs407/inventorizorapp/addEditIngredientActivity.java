f45303ecf0eedea922a160fffd93863aeb18a00a
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-06 10:20:31 -0600

    added basic logic for save ingredient button

--- app/src/main/java/com/cs407/inventorizorapp/addEditIngredientActivity.java
+++ app/src/main/java/com/cs407/inventorizorapp/addEditIngredientActivity.java
@@ -2,17 +2,32 @@ package com.cs407.inventorizorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class addEditIngredientActivity extends AppCompatActivity {
+    Button saveIngredientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_ingredient);
+
+        saveIngredientButton = findViewById(R.id.saveIngredientButton);
+        saveIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("INFO", "Save button pressed");
+                goToMainActivity();
+            }
+        });
     }
 
     public void goToMainActivity() {
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }

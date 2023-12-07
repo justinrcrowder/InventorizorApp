d4310ef9e9b56e006ed4e4d1f2d9bac689fbdcab
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-07 16:17:30 -0600

    cleaning and comments

--- app/src/main/java/com/cs407/inventorizorapp/MainActivity.java
+++ app/src/main/java/com/cs407/inventorizorapp/MainActivity.java
@@ -1,27 +1,20 @@
 package com.cs407.inventorizorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static ArrayList<Ingredient> ingredients;
     DBHelper dbHelper;
     ArrayAdapter adapter;
-
     Button addIngredientButton;
 
     @Override
@@ -29,36 +22,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // load ingredients from the database and prepare them as strings
         Context context = getApplicationContext();
         SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("restaurant", Context.MODE_PRIVATE, null);
         dbHelper = new DBHelper(sqLiteDatabase);
         ingredients = dbHelper.readIngredients();
-
         ArrayList<String> displayIngredients = new ArrayList<>();
         for (Ingredient ingredient : ingredients) {
             displayIngredients.add(String.format("Name:%s\nAmount Owned:%s\tOwned:%s", ingredient.getIngredientName(), ingredient.getAmountOwned(), ingredient.getTargetAmount()));
         }
 
+        // load displayIngredients into listview for display
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayIngredients);
         ListView ingredientsListView = findViewById(R.id.ingredientListView);
         ingredientsListView.setAdapter(adapter);
 
         addIngredientButton = findViewById(R.id.addIngredientButton);
-        addIngredientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("INFO", "addIngredientButton pressed!");
-                goToAddEditIngredient();
-            }
+        addIngredientButton.setOnClickListener(view -> {
+            goToAddEditIngredient();
         });
 
-        ingredientsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(), IngredientInfo.class);
-//                intent.putExtra("ingredientId", i);
-                startActivity(intent);
-            }
+        ingredientsListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            goToIngredientInfo();
         });
     }
 
@@ -67,14 +52,16 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void goToIngredientInfo(String s) {
-
+    public void goToIngredientInfo() {
+        Intent intent = new Intent(getApplicationContext(), IngredientInfo.class);
+        startActivity(intent);
     }
 
     //Might not need this function-- depends on how the camera screen is setup
     public void goToCameraMode() {
 
     }
+
     // Adding a comment on Line 27 of MainActivity for testing Pushes
     public void goToRestaurantProfile() {
         Intent intent = new Intent(this, restaurant_profile.class);

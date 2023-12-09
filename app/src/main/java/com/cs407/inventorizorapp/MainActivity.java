358018a1461e1b137bf09d23ab725d9656c7d909
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-09 15:29:07 -0600

    implemented intent ingredient passing instead of database, added text for when no ingredients displayed

--- app/src/main/java/com/cs407/inventorizorapp/MainActivity.java
+++ app/src/main/java/com/cs407/inventorizorapp/MainActivity.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     public static ArrayList<Ingredient> ingredients;
+    public static ArrayList<String> displayIngredients;
     DBHelper dbHelper;
     ArrayAdapter adapter;
     Button addIngredientButton;
@@ -26,34 +27,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // load ingredients from the database and prepare them as strings
-        Context context = getApplicationContext();
-        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("restaurant", Context.MODE_PRIVATE, null);
-        dbHelper = new DBHelper(sqLiteDatabase);
-        ingredients = dbHelper.readIngredients();
-        ArrayList<String> displayIngredients = new ArrayList<>();
-        for (Ingredient ingredient : ingredients) {
-            displayIngredients.add(String.format("Name:%s\nAmount Owned:%s\tOwned:%s", ingredient.getIngredientName(), ingredient.getAmountOwned(), ingredient.getTargetAmount()));
+//        uncomment if switching back to database
+//        load ingredients from the database and prepare them as strings
+//        Context context = getApplicationContext();
+//        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("restaurant", Context.MODE_PRIVATE, null);
+//        dbHelper = new DBHelper(sqLiteDatabase);
+//        ingredients = dbHelper.readIngredients();
+
+        // fill ingredients from intent
+        if (getIntent() != null) {
+                Log.i("INFO", "Ingredients received!");
+                ingredients = (ArrayList<Ingredient>) getIntent().getSerializableExtra("Ingredient_list");
+        } else {
+            ingredients = new ArrayList<>();
         }
 
-        // load displayIngredients into listview for display
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayIngredients);
-        ListView ingredientsListView = findViewById(R.id.ingredientListView);
-        ingredientsListView.setAdapter(adapter);
+        if (ingredients != null && ingredients.size() > 0) {
+            findViewById(R.id.emptyMessage).setVisibility(View.GONE);
+            displayIngredients = new ArrayList<>();
+            for (Ingredient ingredient : ingredients) {
+                displayIngredients.add(String.format("Name:%s\nAmount Owned:%s\tOwned:%s", ingredient.getIngredientName(), ingredient.getAmountOwned(), ingredient.getTargetAmount()));
+            }
+
+            // load displayIngredients into listview for display
+            adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayIngredients);
+            ListView ingredientsListView = findViewById(R.id.ingredientListView);
+            ingredientsListView.setAdapter(adapter);
+
+            ingredientsListView.setOnItemClickListener((adapterView, view, ingredientIndex, l) -> {
+                Ingredient selectedIngredient = ingredients.get(ingredientIndex);
+                goToIngredientInfo(selectedIngredient);
+            });
+        } else {
+            findViewById(R.id.ingredientListView).setVisibility(View.GONE);
+            findViewById(R.id.emptyMessage).setVisibility(View.VISIBLE);
+        }
 
         addIngredientButton = findViewById(R.id.addIngredientButton);
         addIngredientButton.setOnClickListener(view -> {
             goToAddEditIngredient();
         });
-
-        ingredientsListView.setOnItemClickListener((adapterView, view, ingredientIndex, l) -> {
-            Ingredient selectedIngredient = ingredients.get(ingredientIndex);
-            goToIngredientInfo(selectedIngredient);
-        });
     }
 
     public void goToAddEditIngredient() {
         Intent intent = new Intent(this, addEditIngredientActivity.class);
+        intent.putExtra("Ingredient_list", ingredients);
         startActivity(intent);
     }
 
@@ -62,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("ingredientName", selectedIngredient.getIngredientName());
         intent.putExtra("amountOwned", selectedIngredient.getAmountOwned());
         intent.putExtra("targetAmount", selectedIngredient.getTargetAmount());
+        intent.putExtra("Ingredient_list", ingredients);
         startActivity(intent);
     }
 

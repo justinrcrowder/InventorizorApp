0d39573ecb310ede407b49d80286467b3f29d35e
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-09 15:30:16 -0600

    implemented intents instead of database code for ingredient storage

--- app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
+++ app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
@@ -11,13 +11,18 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class IngredientInfo extends AppCompatActivity {
     Button backButton;
+    public ArrayList<Ingredient> ingredients;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_info);
+
+        ingredients = (ArrayList<Ingredient>) getIntent().getSerializableExtra("Ingredient_list");
 
         // receive information about selected ingredient through intent for display
         Intent intent = getIntent();
@@ -44,11 +49,13 @@ public class IngredientInfo extends AppCompatActivity {
     //Might need two activities for Adding and Editing recipes
     public void goToAddEditIngredient() {
         Intent intent = new Intent(this, addEditIngredientActivity.class);
+        intent.putExtra("Ingredient_list", ingredients);
         startActivity(intent);
     }
 
     public void goToMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Ingredient_list", ingredients);
         startActivity(intent);
     }
 
--- app/src/main/java/com/cs407/inventorizorapp/addEditIngredientActivity.java
+++ app/src/main/java/com/cs407/inventorizorapp/addEditIngredientActivity.java
@@ -16,35 +16,53 @@ import java.util.ArrayList;
 public class addEditIngredientActivity extends AppCompatActivity {
     Button saveIngredientButton;
     public static ArrayList<Ingredient> ingredients;
-    DBHelper dbHelper;
+
+//    uncomment if switching back to database
+//    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_ingredient);
+        ingredients = new ArrayList<>();
 
-        Context context = getApplicationContext();
-        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("restaurant", Context.MODE_PRIVATE, null);
-        dbHelper = new DBHelper(sqLiteDatabase);
-        ingredients = dbHelper.readIngredients();
+//        fill ingredients from intent if exists
+        if (getIntent() != null) {
+            Log.i("INFO", "There are existing ingredients!");
+            ingredients = (ArrayList<Ingredient>) getIntent().getSerializableExtra("Ingredient_list");
+        } else {
+            Log.i("INFO", "No ingredients were passed over...");
+        }
+
+//        uncomment code if switching back to database
+//        Context context = getApplicationContext();
+//        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("restaurant", Context.MODE_PRIVATE, null);
+//        dbHelper = new DBHelper(sqLiteDatabase);
+//        ingredients = dbHelper.readIngredients();
 
         saveIngredientButton = findViewById(R.id.saveIngredientButton);
-        saveIngredientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i("INFO", "Save button pressed");
+        saveIngredientButton.setOnClickListener(view -> {
+            Log.i("INFO", "Save button pressed");
+            if (ingredients != null) {
                 String ingredientName = "Ingredient " + (ingredients.size() + 1);
                 Ingredient newIngredient = new Ingredient(ingredientName, 0, 0);
                 ingredients.add(newIngredient);
                 Log.i("INFO", "New ingredient added: " + newIngredient.getIngredientName());
-                dbHelper.insertIngredient(newIngredient);
-                goToMainActivity();
+//                dbHelper.insertIngredient(newIngredient);
+            } else {
+                ingredients = new ArrayList<>();
+                String ingredientName = "Ingredient " + (ingredients.size() + 1);
+                Ingredient newIngredient = new Ingredient(ingredientName, 0, 0);
+                ingredients.add(newIngredient);
+                Log.i("INFO", "New ingredient added: " + newIngredient.getIngredientName());
             }
+            goToMainActivity();
         });
     }
 
     public void goToMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Ingredient_list", ingredients);
         startActivity(intent);
     }
 }

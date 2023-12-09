233905e58bbe81a176dd57be5fae1be42ef1a8f0
Yusuf-Ergul <yergul@wisc.edu>
2023-12-09 16:53:24 -0600

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 2017607cda318abcda19ae0af98f1e4c871f7d21, reversing
    changes made to 2228818f1dc25e7eac96ef8ea77b107093555146.

--- app/src/main/java/com/cs407/inventorizorapp/addEditIngredientActivity.java
+++ app/src/main/java/com/cs407/inventorizorapp/addEditIngredientActivity.java
@@ -10,21 +10,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
 public class addEditIngredientActivity extends AppCompatActivity {
     Button saveIngredientButton;
     public static ArrayList<Ingredient> ingredients;
-    private String ingredientName;
-    private int amountOwned;
-    private int targetAmount;
-
 
 //    uncomment if switching back to database
 //    DBHelper dbHelper;
@@ -43,48 +34,6 @@ public class addEditIngredientActivity extends AppCompatActivity {
             Log.i("INFO", "No ingredients were passed over...");
         }
 
-        TextInputEditText ingredientNameEditText = findViewById(R.id.ingredientNameEditText);
-        TextView amountOwnedText = findViewById(R.id.amountOwnedText);
-        TextView targetAmountText = findViewById(R.id.targetAmountText);
-
-        ingredientName = "";
-        amountOwned = 5;
-        targetAmount = 5;
-
-        ingredientNameEditText.setText(ingredientName);
-        amountOwnedText.setText(String.valueOf(amountOwned));
-        targetAmountText.setText(String.valueOf(targetAmount));
-
-        ImageButton incrAmountOwnedButton = findViewById(R.id.incrAmountOwned);
-        ImageButton decrAmountOwnedButton = findViewById(R.id.decrAmountOwned);
-        ImageButton incrTargetAmountButton = findViewById(R.id.incrTargetAmount);
-        ImageButton decrTargetAmountButton = findViewById(R.id.decrTargetAmount);
-
-        incrAmountOwnedButton.setOnClickListener(v -> {
-            amountOwned++;
-            amountOwnedText.setText(String.valueOf(amountOwned));
-        });
-
-        decrAmountOwnedButton.setOnClickListener(v -> {
-            if (amountOwned > 0) {
-                amountOwned--;
-                amountOwnedText.setText(String.valueOf(amountOwned));
-            }
-        });
-
-        incrTargetAmountButton.setOnClickListener(v -> {
-            targetAmount++;
-            targetAmountText.setText(String.valueOf(targetAmount));
-        });
-
-        decrTargetAmountButton.setOnClickListener(v -> {
-            if (targetAmount > 0) {
-                targetAmount--;
-                targetAmountText.setText(String.valueOf(targetAmount));
-            }
-        });
-
-
 //        uncomment code if switching back to database
 //        Context context = getApplicationContext();
 //        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("restaurant", Context.MODE_PRIVATE, null);
@@ -94,36 +43,21 @@ public class addEditIngredientActivity extends AppCompatActivity {
         saveIngredientButton = findViewById(R.id.saveIngredientButton);
         saveIngredientButton.setOnClickListener(view -> {
             Log.i("INFO", "Save button pressed");
-
-            ingredientName = ingredientNameEditText.getText().toString().trim();
-
-            if (ingredientName.isEmpty()) {
-                showToast("Ingredient name cannot be blank");
-                return;
-            }
-
-            if (targetAmount <= 0 || amountOwned <= 0) {
-                showToast("Quantities must be greater than zero");
-                return;
-            }
-
-            Ingredient newIngredient = new Ingredient(ingredientName, targetAmount, amountOwned);
-
             if (ingredients != null) {
+                String ingredientName = "Ingredient " + (ingredients.size() + 1);
+                Ingredient newIngredient = new Ingredient(ingredientName, 0, 0);
                 ingredients.add(newIngredient);
                 Log.i("INFO", "New ingredient added: " + newIngredient.getIngredientName());
+//                dbHelper.insertIngredient(newIngredient);
             } else {
                 ingredients = new ArrayList<>();
+                String ingredientName = "Ingredient " + (ingredients.size() + 1);
+                Ingredient newIngredient = new Ingredient(ingredientName, 0, 0);
                 ingredients.add(newIngredient);
                 Log.i("INFO", "New ingredient added: " + newIngredient.getIngredientName());
             }
-
             goToMainActivity();
         });
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
     public void goToMainActivity() {

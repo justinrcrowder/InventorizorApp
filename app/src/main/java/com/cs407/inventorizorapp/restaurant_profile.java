9a03659ebab9fb89b6db6dc432003cfad5f37558
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-11 10:24:36 -0600

    show restaurant name on restaurant_profile

--- app/src/main/java/com/cs407/inventorizorapp/restaurant_profile.java
+++ app/src/main/java/com/cs407/inventorizorapp/restaurant_profile.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class restaurant_profile extends AppCompatActivity {
 
+    private TextView restaurantNameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_profile);
+
+        RestaurantManager restaurantManager = RestaurantManager.getInstance();
+
+        restaurantNameTextView = findViewById(R.id.restaurantName);
+        restaurantNameTextView.setText(restaurantManager.getRestaurantName());
     }
 
     //Might end up needing two activities for Adding a recipe and Editing a recipe

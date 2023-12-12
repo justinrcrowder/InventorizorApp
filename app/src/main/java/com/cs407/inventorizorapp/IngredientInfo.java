69fdc4d519db72f3c30793ab8a2a5cee61894bf0
Justin Crowder <justinrcrowder23@gmail.com>
2023-12-11 23:09:24 -0600

    added linking of ingredients and recipes

--- app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
+++ app/src/main/java/com/cs407/inventorizorapp/IngredientInfo.java
@@ -3,7 +3,9 @@ package com.cs407.inventorizorapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ public class IngredientInfo extends AppCompatActivity {
     private Button backButton;
     private Button editButton;
     private ArrayList<Ingredient> ingredients;
+    private ArrayList<Recipe> recipes;
 
     // Use the RestaurantManager to manage ingredients
     private RestaurantManager restaurantManager;
@@ -29,6 +32,8 @@ public class IngredientInfo extends AppCompatActivity {
 
         // Get ingredients and selectedIngredient from RestaurantManager
         ingredients = restaurantManager.getIngredients();
+        recipes = restaurantManager.getRecipes();
+
         Ingredient selectedIngredient = (Ingredient) getIntent().getSerializableExtra("SelectedIngredient");
         ingredientIndex = getIntent().getIntExtra("IngredientIndex", -1);
         Log.i("INFO", "got index: " + ingredientIndex);
@@ -37,13 +42,22 @@ public class IngredientInfo extends AppCompatActivity {
         int amountOwned = selectedIngredient.getAmountOwned();
         int targetAmount = selectedIngredient.getTargetAmount();
 
-        Log.i("INFO", ingredientName + " " + amountOwned + " " + targetAmount);
-
         // show ingredient information from intent
         TextView ingredientNameTextView = findViewById(R.id.ingredientName);
         TextView ingredientThresholdTextView = findViewById(R.id.ingredientThreshold);
         ingredientNameTextView.setText(ingredientName);
         ingredientThresholdTextView.setText("Low amount threshold: " + targetAmount);
+
+        // show recipes that use this ingredient
+        ArrayList<String> recipeNames = new ArrayList<>();
+        for (Recipe recipe : recipes) {
+            if (recipe.getRecipeIngredients().contains(ingredientName)) {
+                recipeNames.add(recipe.getRecipeName());
+            }
+        }
+        ListView recipeListView = findViewById(R.id.recipeListView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, recipeNames);
+        recipeListView.setAdapter(adapter);
 
         editButton = findViewById(R.id.editButton);
         editButton.setOnClickListener(view -> goToEditIngredient(ingredients, selectedIngredient));
